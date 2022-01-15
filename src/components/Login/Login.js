8f13da1bf93d037d23d81c64496a80446a888492
ChiaHui <bornonefreekiss@alum.ccu.edu.tw>
2022-01-15 22:50:50 +0800

    feat:  Building & Using a Custom Context Provider Component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect, useReducer } from 'react'
+import React, { useState, useEffect, useReducer, useContext } from 'react'
 
 import Card from '../UI/Card/Card'
 import classes from './Login.module.css'
 import Button from '../UI/Button/Button'
+import AuthContext from '../../store/auth-context'
+
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
     return { value: action.val, isValid: action.val.includes('@') }
@@ -24,21 +26,22 @@ const passwordReducer = (state, action) => {
 }
 
 const Login = props => {
-  // const [enteredEmail, setEnteredEmail] = useState('')
-  // const [emailIsValid, setEmailIsValid] = useState()
-  // const [enteredPassword, setEnteredPassword] = useState('')
-  // const [passwordIsValid, setPasswordIsValid] = useState()
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false)
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
     value: '',
     isValid: null
   })
-
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
     value: '',
     isValid: null
   })
+
+  const authCtx = useContext(AuthContext)
 
   useEffect(() => {
     console.log('EFFECT RUNNING')
@@ -65,10 +68,16 @@ const Login = props => {
 
   const emailChangeHandler = event => {
     dispatchEmail({ type: 'USER_INPUT', val: event.target.value })
+
+    // setFormIsValid(
+    //   event.target.value.includes('@') && passwordState.isValid
+    // );
   }
 
   const passwordChangeHandler = event => {
     dispatchPassword({ type: 'USER_INPUT', val: event.target.value })
+
+    // setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
   }
 
   const validateEmailHandler = () => {
@@ -81,7 +90,7 @@ const Login = props => {
 
   const submitHandler = event => {
     event.preventDefault()
-    props.onLogin(emailState.value, passwordState.value)
+    authCtx.onLogin(emailState.value, passwordState.value)
   }
 
   return (
